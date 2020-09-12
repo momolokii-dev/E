e7b243585d460f95649def02467a5f6e8a49aebf
momolokii-dev <didy1234567@gmail.com>
2020-09-12 22:59:14 +0200

    create p

---
+++ app.js
@@ -0,0 +1,52 @@
+let openBtn = document.getElementById("menu-btn");
+let closeBtn = document.getElementById("close-btn");
+
+openBtn.onclick = function () {
+  document.getElementById("side-menu").style.display = "block";
+  document.getElementById("menu-btn").style.display = "none";
+  document.getElementById("close-btn").style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  document.getElementById("side-menu").style.display = "none";
+  document.getElementById("menu-btn").style.display = "block";
+  document.getElementById("close-btn").style.display = "none";
+};
+
+const buttons = document.getElementById("buttons").children;
+const items = document.getElementById("items").children;
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    for (let j = 0; j < buttons.length; j++) {
+      buttons[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target");
+
+    for (let k = 0; k < items.length; k++) {
+      items[k].style.display = "none";
+
+      if (items[k].getAttribute("data-id") == target) {
+        items[k].style.display = "block";
+      }
+      if (target == "all") {
+        items[k].style.display = "block";
+      }
+    }
+  });
+}
+
+let filter = document.getElementById("filter-search");
+
+filter.value = "";
+filter.addEventListener("keyup", function (e) {
+  const term = e.target.value;
+  for (let n = 0; n < items.length; n++) {
+    items[n].style.display = "none";
+
+    if (items[n].getAttribute("data-search") == term) {
+      items[n].style.display = "block";
+    }
+  }
+});
